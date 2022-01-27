83a318b4068cfb60833e43cea70994631256a0d6
isaacmyers11 <90308039@ep-student.org>
2022-01-27 15:56:39 -0600

    Added Turret command and subsystem files

---
+++ src/main/java/com/team2502/robot2022/subsystems/TurretSubsystem.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.team2502.robot2022.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class TurretSubsystem extends SubsystemBase {
+
+  public TurretSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
