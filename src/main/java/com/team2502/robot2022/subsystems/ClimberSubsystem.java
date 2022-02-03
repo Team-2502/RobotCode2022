d8b95b7d1460ab445067dc213282af8f5a4edd0a
Rohit Patil <90308603@ep-student.org>
2022-02-03 17:48:43 -0600

    removed comment

--- src/main/java/com/team2502/robot2022/subsystems/ClimberSubsystem.java
+++ src/main/java/com/team2502/robot2022/subsystems/ClimberSubsystem.java
@@ -29,37 +29,3 @@ public class ClimberSubsystem extends SubsystemBase {
     public boolean isRetracted() { return climberSolenoid.get(); }
 
 }
-
-/*
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.team2502.robot2021.Constants;
-import edu.wpi.first.wpilibj.Solenoid;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import static com.team2502.robot2021.Constants.RobotMap.Motors;
-
-public class ClimberSubsystem extends SubsystemBase {
-    private final WPI_TalonSRX climberMotor;
-    private final Solenoid climberSolenoid;
-
-    public ClimberSubsystem() {
-        climberMotor = new WPI_TalonSRX(Motors.CLIMBER);
-        climberSolenoid = new Solenoid(Constants.RobotMap.Solenoid.CLIMBER);
-        deploySolenoid();
-    }
-
-    public void runClimber(double speed) { climberMotor.set(speed); }
-
-    public void stopClimber() { climberMotor.set(0);}
-
-    public void retractSolenoid() { climberSolenoid.set(true); }
-
-    public void deploySolenoid() {
-        climberSolenoid.set(false);
-    }
-
-    public boolean isRetracted() { return climberSolenoid.get(); }
-
-}
-
- */
