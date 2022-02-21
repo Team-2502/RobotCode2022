b2ccbf6dddfceee524a8705fd42be6779a1e9e2e
Sha-dos <ndpete2007@gmail.com>
2022-02-21 17:14:23 -0600

    updated robot container, also the 100th commit :)

--- src/main/java/com/team2502/robot2022/RobotContainer.java
+++ src/main/java/com/team2502/robot2022/RobotContainer.java
@@ -14,8 +14,6 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.Joystick;
 import com.kauailabs.navx.frc.AHRS;
-import frc.robot.commands.*;
-import frc.robot.subsystems.*;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
