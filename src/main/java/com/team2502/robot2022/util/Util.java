cef07a18134160215a051e5c255c4a5edd6612ad
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2022-02-21 19:12:25 -0600

    added methods to compute distance to the target

--- src/main/java/com/team2502/robot2022/util/Util.java
+++ src/main/java/com/team2502/robot2022/util/Util.java
@@ -27,4 +27,39 @@ public class Util {
 		return val - frictionVal;
         }
     }
+
+    /**
+    * Converts a pixel value to an angle off the center of the camera
+    * @param fov camera field of view in the wanted axis
+    * @param resolution camera resolution in the wanted axis
+    * @param pixel pixel on wanted axis to convert to degrees
+    * @return offset of pixel from center of camera
+     */
+    public static double angleFromPixel(double fov, double resolution, double pixel) {
+	    return Math.toDegrees(Math.atan(
+		    (2 * pixel *
+			Math.tan(
+			  Math.toRadians(fov/4))
+			)
+		    /
+		    resolution
+		    )
+	    );
+    }
+
+    /**
+    * Get distance to target from elevation using trigonometry
+    * @param camHeight distance from ground to camera aperture
+    * @param camElevation angle of camera
+    * @param basketHeight distance from ground to basket vision targets
+    * @param targetElevation angle of target relative to camera
+    * @return distance to target
+     */
+    public static double findDist(double camHeight, double camElevation, double basketHeight, double targetElevation) {
+	    return (
+			    basketHeight - camHeight
+			    /
+			    Math.tan(Math.toRadians(targetElevation+ camElevation))
+		   );
+    }
 }
