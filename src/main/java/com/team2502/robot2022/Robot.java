42310baa7abe5f6df90537eb8ce0486606454a2b
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2022-04-16 13:12:45 -0500

    disabled livewindow, causing overruns

--- src/main/java/com/team2502/robot2022/Robot.java
+++ src/main/java/com/team2502/robot2022/Robot.java
@@ -6,6 +6,7 @@ package com.team2502.robot2022;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -32,6 +33,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    LiveWindow.disableAllTelemetry();
     //new Solenoid(PneumaticsModuleType.REVPH, Constants.RobotMap.Solenoids.INTAKE);
     //new IntakeSubsystem().retractIntake();
   }
